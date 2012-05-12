11906f15e15f2e7315f3f57f745d4a860a237cdd
Matthew Campbell <mattcampbell@pobox.com>
2012-05-12 16:21:36 -0500

    Add locale-sensitive date formatting for jingo

--- witness/helpers.py
+++ witness/helpers.py
@@ -2,8 +2,14 @@ import jinja2
 
 from jingo import register
 
+from babel import dates
+
 import markdown as md
 
 @register.filter
 def markdown(s):
     return jinja2.Markup(md.markdown(unicode(s)))
+
+@register.filter
+def date(t):
+    return dates.format_date(t)
