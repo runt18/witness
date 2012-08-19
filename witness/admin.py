1fc0790c159723fa66aba5b341f46f4da831056e
Joe Chen <khchen428@gmail.com>
2012-08-19 16:41:34 -0700

    Add clone&modify and retire actions to document versions

--- witness/admin.py
+++ witness/admin.py
@@ -3,12 +3,54 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from django.contrib import admin
+from django.core import urlresolvers
+from django.shortcuts import redirect
+
 from witness.models import Document, DocumentVersion, Decision
 
 # Globally disable delete selected
 admin.site.disable_action('delete_selected')
 
+
+
 class DocumentVersionAdmin(admin.ModelAdmin):
+    def clone_and_modify(self, request, queryset):
+        ''' 
+        Clone an existing document version, and redirect to the change page
+        '''
+        try:
+            original_dv = queryset.get()
+            new_dv = DocumentVersion(
+                    document=original_dv.document,
+                    title=original_dv.title,
+                    text=original_dv.text,
+                    yes_action_text=original_dv.yes_action_text,
+                    no_action_text=original_dv.no_action_text
+                )
+            new_dv.save()
+            return redirect(
+                    urlresolvers.reverse(
+                        'admin:witness_documentversion_change',
+                        args=(new_dv.id,)
+                        )
+                )
+        except DocumentVersion.MultipleObjectsReturned:
+            ''' Multiple versions are checked '''
+            self.message_user(
+                    request, 
+                    "Only one document version can be selected"
+                )
+
+    def retire(self, request, queryset):
+        queryset.update(is_retired=True)
+        self.message_user(
+                request, 
+                "Successfully retired %s document version(s)" % 
+                    queryset.count()
+            )
+
+    actions = (clone_and_modify, retire)
+
     def has_change_permission(self, request, obj=None):
         ''' 
         If a document version has been signed,
